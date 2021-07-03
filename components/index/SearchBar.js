fd7f3d3344b1aa27f0ab2c0ae4b5e5dfa27d2c60
Michael Morrison <morrim5@rpi.edu>
2021-07-03 16:55:26 -0400

    search bar input padding

--- components/index/SearchBar.js
+++ components/index/SearchBar.js
@@ -5,7 +5,7 @@ function SearchBar() {
     <div className="bg-white w-3/4 max-w-sm p-2 rounded-full flex border border-black-20">
       <SearchIcon className="text-black-20 px-2 w-16" />
       <input
-        className="w-full rounded"
+        className="w-full rounded mx-2"
         type="text"
         placeholder="Search Documentation"
       />
