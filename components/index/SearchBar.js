913be58054cdd228aa9ce0d0b8faaf484916ed7b
Michael Morrison <morrim5@rpi.edu>
2021-07-03 16:47:49 -0400

    heroicon fix

--- components/index/SearchBar.js
+++ components/index/SearchBar.js
@@ -3,7 +3,7 @@ import { SearchIcon } from "@heroicons/react/solid";
 function SearchBar() {
   return (
     <div className="bg-white w-3/4 max-w-sm p-2 rounded-full flex border border-black-20">
-      <SearchIcon className="text-black-20 px-2" />
+      <SearchIcon className="text-black-20 px-2 w-16" />
       <input
         className="w-full rounded"
         type="text"
