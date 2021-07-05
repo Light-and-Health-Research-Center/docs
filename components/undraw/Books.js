04878ad0dab0653250eaf87852c3e615b691613f
Michael Morrison <morrim5@rpi.edu>
2021-07-05 16:33:50 -0400

    added eslint - book svg alt tag

--- components/undraw/Books.js
+++ components/undraw/Books.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import books from "../../public/img/undraw/books.svg";
 
 export default function Books() {
-  return <Image src={books} width={400} height={400} />;
+  return <Image src={books} width={400} height={400} alt="Lady on books" />;
 }
