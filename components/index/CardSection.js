1e4aa90387ea0fb7a60123c983f6b97ea2ddc588
Michael Morrison <morrim5@rpi.edu>
2021-07-05 16:26:36 -0400

    some spacing on index

--- components/index/CardSection.js
+++ components/index/CardSection.js
@@ -3,10 +3,15 @@ import img from "../../public/img/docs/cscalc/card.png";
 
 export default function CardSection({ data }) {
   return (
-    <section className="bg-white-off p-4 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-      {data.map((card) => (
-        <Card key={card.title} card={card} />
-      ))}
+    <section className="bg-white-off p-4 md:p-8 lg:p-12">
+      <div className="text-center">
+        <h6 className="pb-4 md:pb-8 lg:pb-12">Choose a Product</h6>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
+        {data.map((card) => (
+          <Card key={card.title} card={card} />
+        ))}
+      </div>
     </section>
   );
 }
--- components/index/Hero.js
+++ components/index/Hero.js
@@ -2,20 +2,19 @@ import Books from "../undraw/Books";
 
 function Hero() {
   return (
-    <section className="p-4 md:flex md:flex-row gap-16 justify-between md:p-12">
+    <section className="p-4 md:flex flex-row-reverse gap-16 justify-between md:p-12 md:mx-4 lg:mx-12">
+      <div className="content-center w-auto md:w-2/4 md:flex relative">
+        <div className="w-3/6 sm:w-2/6 md:w-auto m-auto overflow-hidden relative">
+          <Books />
+        </div>
+      </div>
       <div className="md:flex">
         <div className="md:m-auto md:mt-12">
           <h5 className="text-barbiePink-100 w-2/4">docs.light-health.org</h5>
           <h1>Light and Health Docs</h1>
           <p>
-            Choose an application below to get started reading its
-            documentation.
+            Choose a product below to get started reading its documentation.
           </p>
-        </div>
-      </div>
-      <div className=" content-center hidden md:flex w-2/4 relative">
-        <div className="w-auto m-auto overflow-hidden relative">
-          <Books />
         </div>
       </div>
     </section>
