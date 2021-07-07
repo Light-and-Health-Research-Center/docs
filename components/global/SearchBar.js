7016a1647eaa1feb9c7519056024ca991ea27de1
Michael Morrison <morrim5@rpi.edu>
2021-07-07 19:01:52 -0400

    mobile fix attempt 6

--- components/global/SearchBar.js
+++ components/global/SearchBar.js
@@ -5,11 +5,11 @@ export default function SearchBar({ center }) {
     <div
       className={`${
         center ? "mx-auto" : ""
-      } group bg-white-100 w-3/4 max-w-sm p-2 rounded-full flex border border-black-20 ms-focus-barbiePink h-min`}
+      } group bg-white-100 w-3/4 max-w-sm p-2 rounded-full flex border border-black-20 ms-focus-barbiePink`}
     >
       <SearchIcon className="text-black-20 px-2 w-16" />
       <input
-        className="appearance-none text-black-60 w-full rounded mx-2 focus:outline-none"
+        className="text-black-60 w-full rounded mx-2 focus:outline-none"
         type="text"
         placeholder="Search Documentation"
       />
