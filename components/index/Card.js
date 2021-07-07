f28a7bfe03d9f883ab4c4bdc1c2856430f23e952
Michael Morrison <morrim5@rpi.edu>
2021-07-07 19:08:22 -0400

    card img size was too big on mobile

--- components/index/Card.js
+++ components/index/Card.js
@@ -8,7 +8,7 @@ export default function Card({ card }) {
       <a className="bg-white-100 rounded-xl drop-shadow-softerBlur cursor-pointer w-full lg:w-1/2-4gap 2xl:w-1/3-4gap transition duration-150 ease-in-out  _hover:hover:-translate-y-1 _hover:hover:scale-102 ms-focus-barbiePink">
         <div className="flex">
           <div className="flex-shrink-0">
-            <div className="relative h-44 w-36">
+            <div className="relative h-40 w-28 xs:h-44 xs:w-36">
               <Image
                 className="rounded-l-xl"
                 src={`${card.card_img}`}
@@ -22,7 +22,7 @@ export default function Card({ card }) {
             <h4 className="w-min truncate transition duration-500 ease-in-out">
               {card.title}
             </h4>
-            <p className="line-clamp-2 transition duration-500 ease-in-out">
+            <p className="line-clamp-3 transition duration-500 ease-in-out">
               {card.desc}
             </p>
             <div className="relative mt-auto w-full">
