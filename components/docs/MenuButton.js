5df41d858337a6b70ce85cdeab6320eaf815eb33
Michael Morrison <morrim5@rpi.edu>
2021-07-07 18:37:17 -0400

    mobile bugged fix attempt 2

--- components/docs/MenuButton.js
+++ components/docs/MenuButton.js
@@ -3,12 +3,12 @@ import { useState } from "react";
 
 export default function MenuButton({ open }) {
   return (
-    <button className="cursor-pointer w-auto h-full bg-white-off rounded-md border border-black-20 ms-focus-barbiePink">
+    <>
       {open ? (
         <XIcon className="h-full my-auto text-barbiePink-100 w-6 mx-3" />
       ) : (
         <MenuIcon className="h-full my-auto text-barbiePink-100 w-6 mx-3" />
       )}
-    </button>
+    </>
   );
 }
