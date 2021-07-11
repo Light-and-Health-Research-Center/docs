707f16bc5c015d3e813565916adab4b3546cfbc0
Michael Morrison <morrim5@rpi.edu>
2021-07-11 17:02:51 -0400

    testing husky

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,5 +1,10 @@
+import { docsCache } from "../../cache/docs";
+
 export default (req, res) => {
+  const results = req.query.q
+    ? docsCache.filter((doc) => doc.title.toLowerCase().includes(req.query.q))
+    : [];
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ results: [] }));
+  res.end(JSON.stringify({ results }));
 };
