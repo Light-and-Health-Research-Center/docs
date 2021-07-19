8ca09cccd1a8bfb2acf55efbe11ec6e85c780db0
Michael Morrison <morrim5@rpi.edu>
2021-07-19 11:15:59 -0400

    remove smooth scroll for now

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,5 @@
 import "../styles/globals.css";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-
-function useNormalScrollRoutes() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.events.on("routeChangeStart", () => {
-      document.documentElement.classList.add("normal-scroll");
-    });
-    router.events.on("routeChangeComplete", () => {
-      document.documentElement.classList.remove("normal-scroll");
-    });
-  }, []);
-}
 
 export default function MyApp({ Component, pageProps }) {
-  useNormalScrollRoutes();
   return <Component {...pageProps} />;
 }
