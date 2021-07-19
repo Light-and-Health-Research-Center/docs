7e74a8b888de9573420e93ce5b9c2656e05e8ee1
Michael Morrison <morrim5@rpi.edu>
2021-07-19 00:21:23 -0400

    fix smooth scrool next js conflict

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+function useNormalScrollRoutes() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      document.documentElement.classList.add("normal-scroll");
+    });
+    router.events.on("routeChangeComplete", () => {
+      document.documentElement.classList.remove("normal-scroll");
+    });
+  }, []);
+}
 
 export default function MyApp({ Component, pageProps }) {
+  useNormalScrollRoutes();
   return <Component {...pageProps} />;
 }
