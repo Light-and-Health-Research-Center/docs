636646ed6e350066b0141f29d6e68f20e7d1d192
Michael Morrison <morrim5@rpi.edu>
2021-08-05 15:23:41 -0400

    cs calc background init

--- components/markdown/References.js
+++ components/markdown/References.js
@@ -1,10 +1,15 @@
-export function Reference({ children }) {
-  return <p className="text-sm">{children}</p>;
+export function Reference({ num, children }) {
+  return (
+    <p className="text-sm">
+      {num && <span className="font-bold mr-2">{num}.</span>}
+      {children}
+    </p>
+  );
 }
 
 export function References({ children }) {
   return (
-    <div className="bg-white-off p-2 rounded-md m-4 border ">
+    <div className="bg-white-off p-2 rounded-md m-4 border">
       <h5>References:</h5>
       <div>{children}</div>
     </div>
